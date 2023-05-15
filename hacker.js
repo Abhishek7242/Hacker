36329683de1bf0b9caa90246c992c866555668b8
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2023-05-15 12:30:56 +0530

    HACKER PRANK
    JUST UPLOADED

---
+++ hacker.js
@@ -0,0 +1,179 @@
+
+
+
+
+cancelIdleCallback=()=>{
+
+
+
+  const downloding ='#Data Downloading';
+
+document.getElementById("download").innerText=downloding
+  
+document.getElementById('img2').style.opacity=1
+  document.getElementById('start').innerText='#Started'
+}
+
+img0=()=>{
+  
+  
+  
+  document.getElementById('img0').style.opacity=1
+  
+}
+img0()
+img=()=>{
+document.getElementById('img1').style.opacity=1
+}
+img3=()=>{
+  
+  document.getElementById('img3').style.opacity=1
+  document.getElementById('img3').style.display='inline'
+}
+
+const textToType ='#Installing virus in System...........';
+const delay = 60; // milliseconds
+
+let index = 0;
+const typingElement = document.getElementById("p1");
+
+function typeNextLetter() {
+  typingElement.innerHTML += textToType[index];
+  index++;
+  if (index < textToType.length) {
+    setTimeout(typeNextLetter, delay);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter();
+});
+
+
+const textToType1 ='#Breaking Browser Protection Shiels  =>.> #  {browser shield removed == True}';
+const delay1 = 10; // milliseconds
+
+let index1 = 0;
+const typingElement1 = document.getElementById("p2");
+
+function typeNextLetter1() {
+  typingElement1.innerHTML += textToType1[index1];
+  index1++;
+  if (index1 < textToType1.length) {
+    setTimeout(typeNextLetter1, delay1);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter1();
+});
+
+const textToType2 ='#Initiating  data downloding program ';
+const delay2 = 30; // milliseconds
+let index2 = 0;
+const typingElement2 = document.getElementById("p3");
+
+function typeNextLetter2() {
+  typingElement2.innerHTML += textToType2[index2];
+  index2++;
+  if (index2 < textToType2.length) {
+    setTimeout(typeNextLetter2, delay2);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter2();
+});
+const textToType3 ='# Searching private data in phone == {true} #////hacked ==/confirmed ; ' ;
+const delay3 = 20; // milliseconds
+let index3 = 0;
+const typingElement3 = document.getElementById("p4");
+
+function typeNextLetter3() {
+  typingElement3.innerHTML += textToType3[index3];
+  index3++;
+  if (index3 < textToType3.length) {
+    setTimeout(typeNextLetter3, delay3);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter3();
+});
+const textToType4 =`# const textToType4 = ;
+const delay4 = 30; // milliseconds
+let index4 = 0;
+const typingElement4 = document.getElementById("p5");
+
+function typeNextLetter4() {
+  typingElement4.innerHTML += textToType4[index4];
+  index4++;
+  if (index4 < textToType4.length) {
+    setTimeout(typeNextLetter4, delay4);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter4();
+}); ; `  ;
+const delay4 = 10; // milliseconds
+let index4 = 0;
+const typingElement4 = document.getElementById("p5");
+
+function typeNextLetter4() {
+  typingElement4.innerHTML += textToType4[index4];
+  index4++;
+  if (index4 < textToType4.length) {
+    setTimeout(typeNextLetter4, delay4);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter4();
+});
+const textToType5 =`def complicated_algorithm(matrix):
+n = len(matrix)
+m = len(matrix[0])
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + matrix[i-1][j-1]
+
+return dp[n][m]`
+const delay5 = 10; // milliseconds
+let index5 = 0;
+const typingElement5 = document.getElementById("p6");
+
+function typeNextLetter5() {
+  typingElement5.innerHTML += textToType5[index5];
+  index5++;
+  if (index5 < textToType5.length) {
+    setTimeout(typeNextLetter5, delay5);
+  }
+}
+
+// Start typing after page loads
+window.addEventListener("load", function() {
+  typeNextLetter5();
+});
+setTimeout(()=>{
+
+  img()
+},4000)
+setTimeout(()=>{
+  cancelIdleCallback()
+  img3()
+},6000)
+
+
+
+
+
